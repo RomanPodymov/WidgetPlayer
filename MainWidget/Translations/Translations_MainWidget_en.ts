33a3aaaf56fb2340c39a81b2bcb2efde080bfc32
Roman Podymov <podymfrombryansk@yandex.com>
2023-11-12 20:58:19 +0100

    Translations

--- MainWidget/Translations/Translations_MainWidget_en.ts
+++ MainWidget/Translations/Translations_MainWidget_en.ts
@@ -4,10 +4,12 @@
 <context>
     <name>MainWidget</name>
     <message>
+        <location filename="../mainwidget.cpp" line="86"/>
         <source>Loading</source>
         <translation>Loading</translation>
     </message>
     <message>
+        <location filename="../mainwidget.cpp" line="96"/>
         <source>Error</source>
         <translation>Error</translation>
     </message>
